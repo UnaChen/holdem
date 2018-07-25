aa7c01655d70e67d0e4a33fefc7053b3af9effd0
Mick Zhuang <mickjuang@gmail.com>
2018-07-25 09:48:54 +0800

    add hodorAgent

--- holdem/utils.py
+++ holdem/utils.py
@@ -36,7 +36,7 @@ lastsidepot, (numer), resolve when someone all in  <NOT USING NOW>
 reloadCount, (number), only used when TM's version <ONLY TM USED>
 hand, (list), information about two card <IN TREYS FORMAT>
 '''
-COMMUNITY_STATE = namedtuple('community_state', ['button', 'smallblind', 'bigblind', 'totalpot', 'lastraise', 'call_price', 'to_call', 'current_player'])
+COMMUNITY_STATE = namedtuple('community_state', ['button', 'smallblind', 'bigblind', 'totalpot', 'lastraise', 'call_price', 'to_call', 'current_player', 'round'])
 '''
 button, (id), the id of bigblind {e.g. button(id=0), smallblind(id=1), bigblind(id=2) }
 smallblind, (number), the current small blind amount
